ade9587f89ea154638eb33adffccc1ab5d86ddfe
nithyabl <nithyashreebl36@gmail.com>
2026-04-07 17:15:47 +0530

    added prop types

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from 'react-router-dom'
 import { useEffect } from 'react'
+import PropTypes from 'prop-types'
 
 const navItems = [
     { path: '/', label: 'Home', icon: 'M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-4 0a1 1 0 01-1-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 01-1 1' },
@@ -89,6 +90,11 @@ const Drawer = ({ isOpen, onClose }) => {
             </aside>
         </>
     )
+}
+
+Drawer.propTypes = {
+    isOpen: PropTypes.bool.isRequired,
+    onClose: PropTypes.func.isRequired,
 }
 
 export default Drawer
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { NavLink } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
 const navItems = [
     { path: '/', label: 'Home', icon: 'M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-4 0a1 1 0 01-1-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 01-1 1' },
@@ -60,6 +61,10 @@ const Navbar = ({ onMenuClick }) => {
             </div>
         </nav>
     )
+}
+
+Navbar.propTypes = {
+    onMenuClick: PropTypes.func.isRequired,
 }
 
 export default Navbar
